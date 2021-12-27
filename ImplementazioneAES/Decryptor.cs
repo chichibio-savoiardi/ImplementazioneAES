2a7c4d5ea9114a42c3bf9d470e1918f2c61f0394
iuri <iuricube@gmail.com>
2021-12-27 15:10:42 +0100

    salvo per dopo

---
+++ ImplementazioneAES/Decryptor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementazioneAES
+{
+    internal class Decryptor
+    {
+        internal byte InvSubBytes(byte[,] state)
+        {
+            //TODO
+            return 0x00;
+        }
+
+        internal byte InvShiftRows(byte[,] state)
+        {
+            //TODO
+            return 0x00;
+        }
+
+        internal byte InvMixColumns(byte[,] state)
+        {
+            //TODO
+            return 0x00;
+        }
+
+        internal byte InvAddRoundKey(byte[,] state, byte[,] key)
+        {
+            //TODO
+            return 0x00;
+        }
+    }
+}
---
+++ ImplementazioneAES/Encryptor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImplementazioneAES;
+
+namespace ImplementazioneAES
+{
+    /// <summary>
+    /// Esegue la crittografia di una matrice di byte
+    /// </summary>
+    internal class Encryptor
+    {
+        private byte[,] state;
+        private byte[,] key;
+        public Encryptor(byte[,] stateIn, byte[,] keyIn)
+        {
+            state = stateIn;
+            key = keyIn;
+        }
+
+        public byte[,] Encrypt()
+        {
+
+            return state;
+        }
+
+        private byte[,] SubBytes(byte[,] state)
+        {
+            //TODO
+            return state;
+        }
+
+        private byte[,] ShiftRows(byte[,] state)
+        {
+            //TODO
+            return state;
+        }
+
+        private byte[,] MixColumns(byte[,] state)
+        {
+            //TODO
+            return state;
+        }
+
+        private byte[,] AddRoundKey(byte[,] state, byte[,] key)
+        {
+            //TODO
+            return state;
+        }
+    }
+}
