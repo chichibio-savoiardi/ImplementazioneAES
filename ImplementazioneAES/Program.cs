6c950c88854e07a6384e49346e182c4306aabfde
iuri <iuricube@gmail.com>
2021-12-28 15:04:52 +0100

    Interazione di base con il programma

--- ImplementazioneAES/Program.cs
+++ ImplementazioneAES/Program.cs
@@ -1,10 +1,32 @@
 ﻿namespace ImplementazioneAES
 {
-    public class Program
+    public static class Program
     {
+        public static string HelpStr { get; private set; } =
+            "Strumento di criptazione e decriptazione di file con algoritmo AES128.\n" +
+            "Operazioni possibili:\n" +
+            "  encrypt <file> : criptazione file\n" +
+            "  decrypt <file> : decriptazione file\n";
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Troppi pochi argomenti, il programma necessita dell'operazione scelta e di un file.\nEs: '.\\ImplementazioneAES.exe encrypt file.txt'\n" + Program.HelpStr);
+                return;
+            }
+
+            switch (args[0])
+            {
+                case "encrypt":
+                    Console.WriteLine("TODO, encrypt file " + args[1]);
+                    break;
+                case "decrypt":
+                    Console.WriteLine("TODO, decrypt file " + args[1]);
+                    break;
+                default:
+                    Console.WriteLine("Operazione non supportata.\n" + Program.HelpStr);
+                    break;
+            }
         }
     }
 }
