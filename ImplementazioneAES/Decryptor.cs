4c1ef1b8c106c04b9d42edb53342d055734d92a4
iuri <iuricube@gmail.com>
2022-01-04 16:25:24 +0100

    Ultimi aggiustamenti

--- ImplementazioneAES/Decryptor.cs
+++ ImplementazioneAES/Decryptor.cs
@@ -27,9 +27,9 @@ namespace ImplementazioneAES
             List<byte[]> list = new List<byte[]>(len + 1);
 
             list.Add(state[0..4]);
-            list.Add(ShiftRight(state[4..8], 1));
-            list.Add(ShiftRight(state[8..12], 2));
-            list.Add(ShiftRight(state[12..16], 3));
+            list.Add(Utility.ShiftRight(state[4..8], 1));
+            list.Add(Utility.ShiftRight(state[8..12], 2));
+            list.Add(Utility.ShiftRight(state[12..16], 3));
 
             byte[] output = new byte[len];
 
@@ -42,30 +42,6 @@ namespace ImplementazioneAES
                 }
             }
 
-            return output;
-        }
-
-        private static byte[] ShiftRight(byte[] arr, int times)
-        {
-            byte[] ShiftRightOne(byte[] arr)
-            {
-                int len = arr.Length;
-                byte[] output = new byte[len];
-                byte tmp = arr[len - 1];
-                for (int i = len - 1; i > 0; i--)
-                {
-                    output[i] = arr[i - 1];
-                }
-                output[0] = tmp;
-
-                return output;
-            }
-
-            byte[] output = (byte[])arr.Clone();
-            for (int i = 0; i < times; i++)
-            {
-                output = ShiftRightOne(output);
-            }
             return output;
         }
 
